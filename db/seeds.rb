ea6a33f80d5837bda9764ea73a88fd1d49569945
David burton <davidburton@Davids-MacBook-Air-2.local>
2017-01-26 20:20:04 -0500

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,33 +6,38 @@ require_relative "../app/models/stat.rb"
 
 
 
-	Skill.create(:name => "free throw percentage", :level_of_difficulty => 6)
-	Stat.create(:name => "free throws attempted", :skill_id => 1)
-	Stat.create(:name => "free throws made", :skill_id => 1)	
+# 	Skill.create(:name => "free throw percentage", :level_of_difficulty => 6)
+# 	Stat.create(:name => "free throws attempted", :skill_id => 1)
+# 	Stat.create(:name => "free throws made", :skill_id => 1)	
 
-	Skill.create(:name => "ball handeling", :level_of_difficulty => 7)	
-	Stat.create(:name => "number of weak hand dribbles per minute", :skill_id => 2)	
-	Stat.create(:name => "number of strong hand dribbles per minute", :skill_id => 2)
+# 	Skill.create(:name => "ball handeling", :level_of_difficulty => 7)	
+# 	Stat.create(:name => "number of weak hand dribbles per minute", :skill_id => 2)	
+# 	Stat.create(:name => "number of strong hand dribbles per minute", :skill_id => 2)
 
-	Skill.create(:name => "three point percentage", :level_of_difficulty => 10)	
-	Stat.create(:name => "number of three pointers attempted", :skill_id => 3)	
-	Stat.create(:name => "number of three pointers made", :skill_id => 3)	
+# 	Skill.create(:name => "three point percentage", :level_of_difficulty => 10)	
+# 	Stat.create(:name => "number of three pointers attempted", :skill_id => 3)	
+# 	Stat.create(:name => "number of three pointers made", :skill_id => 3)	
 
-	Skill.create(:name => "mid range percentage", :level_of_difficulty => 7)	
-	Stat.create(:name => "jump shots made", :skill_id => 5)	
-	Stat.create(:name => "jump shots attempted", :skill_id => 5)
-	Stat.create(:name => "lay ups  attempted", :skill_id => 5)
-	Stat.create(:name => "lay ups made", :skill_id => 5)
+# 	Skill.create(:name => "mid range percentage", :level_of_difficulty => 7)	
+# 	Stat.create(:name => "jump shots made", :skill_id => 5)	
+# 	Stat.create(:name => "jump shots attempted", :skill_id => 5)
+# 	Stat.create(:name => "lay ups  attempted", :skill_id => 5)
+# 	Stat.create(:name => "lay ups made", :skill_id => 5)
 
 
-	Skill.create(:name => "illegal plays", :level_of_difficulty => 8)	
-	Stat.create(:name => "turnovers", :skill_id => 6)	
-	Stat.create(:name => "reach in's made", :skill_id => 6)
-	Stat.create(:name => "hand checks made", :skill_id => 6)
-	Stat.create(:name => "blocking foul made", :skill_id => 6)	
+# 	Skill.create(:name => "illegal plays", :level_of_difficulty => 8)	
+# 	Stat.create(:name => "turnovers", :skill_id => 6)	
+# 	Stat.create(:name => "reach in's made", :skill_id => 6)
+# 	Stat.create(:name => "hand checks made", :skill_id => 6)
+# 	Stat.create(:name => "blocking foul made", :skill_id => 6)	
 
-	Stat.create(:name => "stealing the ball", :skill_id => 1)  
-	Stat.create(:name => "assists", :skill_id => 7) 
-	Stat.create(:name => "charges made", :skill_id => 7) 
+# 	Stat.create(:name => "stealing the ball", :skill_id => 1)  
+# 	Stat.create(:name => "assists", :skill_id => 7) 
+# 	Stat.create(:name => "charges made", :skill_id => 7) 
+	
+	Skill.create(:name => "heroic", :level_of_difficulty => 10)
+	Stat.find_by_name("stealing the ball").update_attributes(:skill_id => 8)
+	Stat.find_by_name("assists").update_attributes(:skill_id => 8)
+	Stat.find_by_name("charges made").update_attributes(:skill_id => 8)
 
 
